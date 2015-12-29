873332e0163c7452eb55c233c8cdad70223d19a8
davidgumzchoi <davidgumzchoi@gmail.com>
2015-12-29 03:05:26 -0800

    first commit

---
+++ application.js
@@ -0,0 +1,67 @@
+$(window).load(function () {
+    $("#menu").sticky({topSpacing: 0});
+});
+
+$(function () {
+    var windowH = $(window).height();
+    var bannerH = $('#banner').height();
+    if (windowH > bannerH) {
+        $('#banner').css({'height': ($(window).height() - 68) + 'px'});
+        $('#bannertext').css({'height': ($(window).height() - 68) + 'px'});
+    }
+    $(window).resize(function () {
+        var windowH = $(window).height();
+        var bannerH = $('#banner').height();
+        var differenceH = windowH - bannerH;
+        var newH = bannerH + differenceH;
+        var truecontentH = $('#bannertext').height();
+        if (windowH < truecontentH) {
+            $('#banner').css({'height': (newH - 68) + 'px'});
+            $('#bannertext').css({'height': (newH - 68) + 'px'});
+        }
+        if (windowH > truecontentH) {
+            $('#banner').css({'height': (newH - 68) + 'px'});
+            $('#bannertext').css({'height': (newH - 68) + 'px'});
+        }
+    })
+});
+
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+
+        var target = this.hash;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event) {
+    var scrollPosition = $(document).scrollTop();
+    $('.nav li a').each(function () {
+        var currentLink = $(this);
+        var refElement = $(currentLink.attr("href"));
+        if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
+            $('.nav li a').removeClass("active");
+            currentLink.addClass("active");
+        } else {
+            currentLink.removeClass("active");
+        }
+    });
+}
+
+$('#modal').on('shown.bs.modal', function () {
+	$('#myInput').focus();
+})
